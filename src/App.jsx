0a9ad8d07bf309e03d56d4daf03e671ed476cffb
SadikRa <sadikrahman494@gmail.com>
2025-01-01 19:07:41 +0600

    counter slice file added done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ const App = () => {
     <div className="flex justify-center items-center min-h-screen mx-auto max-w-7xl">
       <div className="text-center">
         <h1>Counter with Redux</h1>
-
         <button className="bg-orange-500 hover:bg-gray-300 p-3 rounded-lg">
           Increment
         </button>
